0d2100bd4fdd5e9e6475a0503eaa4b7faef494fa
AyeshaJaved151 <aishajawaid483@gmail.com>
2026-02-07 13:16:06 -0800

    portfolio

---
+++ app.js
@@ -0,0 +1,30 @@
+document.querySelectorAll(".navbar a.nav-link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+const projectLinks = [
+  "https://ayeshajaved151.github.io/css-assignment-4/",
+  "https://ayeshajaved151.github.io/css-assignment-7/",
+  "https://ayeshajaved151.github.io/css-assignment-3/",
+  "https://ayeshajaved151.github.io/css-assignment-5/",
+  "https://ayeshajaved151.github.io/css-assignment-6/",
+  "https://ayeshajaved151.github.io/hackathon",
+  "https://ayeshajaved151.github.io/Home-Page-1/",
+  "https://ayeshajaved151.github.io/Home-Page-2/",
+  "https://ayeshajaved151.github.io/Femhack-2026/",
+  "https://ayeshajaved151.github.io/E-commerce-UI/",
+  "https://ayeshajaved151.github.io/calculator/",
+  "https://ayeshajaved151.github.io/TODO/",
+];
+
+document.querySelectorAll(".project-btn").forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    window.open(projectLinks[index], "_blank"); 
+  });
+});
